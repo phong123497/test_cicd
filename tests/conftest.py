9a3e6ebc92586ecc458a496fcf3101406a9a6f9e
グエン ズイ フォン <nguyen-duy-phong@keystone.technosmile.co.jp>
2025-08-08 15:57:13 +0900

    fit error pep8

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,5 +6,3 @@ PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 SRC_PATH = os.path.join(PROJECT_ROOT, "src")
 if SRC_PATH not in sys.path:
     sys.path.insert(0, SRC_PATH)
-
-
